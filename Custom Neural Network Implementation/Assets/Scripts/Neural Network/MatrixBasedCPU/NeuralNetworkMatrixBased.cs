4bf4e9ad2a93f0fba420c0deef730ff77f4253bd
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2024-02-15 14:08:59 -0800

    forward propagation, softmax, and sigmoid now all working

--- Custom Neural Network Implementation/Assets/Scripts/Neural Network/MatrixBasedCPU/NeuralNetworkMatrixBased.cs
+++ Custom Neural Network Implementation/Assets/Scripts/Neural Network/MatrixBasedCPU/NeuralNetworkMatrixBased.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NeuralNetworkMatrixBased : MonoBehaviour
 {
@@ -12,45 +14,56 @@ public class NeuralNetworkMatrixBased : MonoBehaviour
     public int outputSize;
 
     [Header("Neural Net Data")]
-    private Vector<Vector<float>> W1;
-    private Vector<Vector<float>> W2;
+    private float [,] W1;
+    private float [,] W2;
 
     private float[] B1;
     private float[] B2;
 
+    private float[] A1;
+    private float[] A2;
+
     void Awake()
     {
         InitializeNetwork();
+        TestNetwork();
     }
 
     private void InitializeNetwork()
     {
-        W1 = new float[hiddenSize,inputSize];
-        W2 = new float[outputSize, hiddenSize];
+        W1 = new float[inputSize, hiddenSize];
+        W2 = new float[hiddenSize, outputSize];
 
         B1 = new float[hiddenSize];
         B2 = new float[outputSize];
 
-        W1V = new Vector<float>(10);
+        A1 = new float[hiddenSize];
+        A2 = new float[outputSize];
 
-
-
-        for (int x = 0; x < hiddenSize; x++)
+        for (int x = 0; x < inputSize; x++)
         {
-            B1[x] = Random.Range(-0.5f, 0.5f);
-            for (int  y = 0; y < inputSize; y++)
+            for (int y = 0; y < hiddenSize; y++)
             {
-                W1[x,y] = Random.Range(-0.5f, 0.5f);
+                W1[x, y] = Random.Range(-0.5f, 0.5f);
             }
         }
 
-        for (int x = 0; x < outputSize; x++)
+        for (int x = 0; x < hiddenSize; x++)
         {
-            B2[x] = Random.Range(-0.5f, 0.5f);
-            for (int y = 0; y < hiddenSize; y++)
+            for (int y = 0; y < outputSize; y++)
             {
                 W2[x, y] = Random.Range(-0.5f, 0.5f);
             }
+        }
+
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            B1[i] = Random.Range(-0.5f, 0.5f);
+        }
+
+        for (int i = 0; i < outputSize; i++)
+        {
+            B2[i] = Random.Range(-0.5f, 0.5f);
         }
 
         Debug.Log("W1 Size: " + W1.Length);
@@ -58,11 +71,54 @@ public class NeuralNetworkMatrixBased : MonoBehaviour
 
         Debug.Log("B1 Size: " + B1.Length);
         Debug.Log("B2 Size: " + B2.Length);
+
+        Debug.Log("Total Connections: " + (W1.Length + W2.Length + B1.Length + B2.Length));
+    }
+
+    private void TestNetwork()
+    {
+        float[] x = new float[inputSize];
+
+        for (int i = 0; i < inputSize; i++)
+        {
+            x[i] = Random.Range(-10, 10);
+        }
+
+        ForwardProp(x);
+        DisplayProbabilities();
+    }
+
+    private void DisplayProbabilities()
+    {
+        for (int i = 0; i < outputSize; i++)
+        {
+            Debug.Log("Probability of class [" + i + "]: " + A2[i]);
+        }
+
+        Debug.Log("Sum of probabilities: " + A2.Sum());
     }
 
     private void ForwardProp(float[] x)
     {
-       
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            for (int  j = 0; j < inputSize; j++)
+            {
+                A1[i] += x[j] * W1[j,i];
+            }
+            A1[i] += B1[i];
+            A1[i] = Sigmoid(A1[i]);
+        }
+
+        for (int i = 0; i < outputSize; i++)
+        {
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                A2[i] += A1[j] * W2[j,i];
+            }
+            A2[i] += B2[i];
+        }
+        A2 = Softmax(A2);
     }
 
     private void BackwardProp()
@@ -73,5 +129,28 @@ public class NeuralNetworkMatrixBased : MonoBehaviour
     private void UpdateNetwork()
     {
 
+    }
+
+    private float[] Softmax(float[] A)
+    {
+        float[] predictions = new float[outputSize];
+        float sum = 0;
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            sum += Mathf.Exp(A[i]);
+        }
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            predictions[i] = Mathf.Exp(A[i]) / sum;
+        }
+
+        return predictions;
+    }
+
+    private float Sigmoid(float x)
+    {
+        return 1 / (1 + Mathf.Exp(-x));
     }
 }
